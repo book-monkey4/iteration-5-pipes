34f15327c176a182d5ca4af7d12e07c29b3fc965
The Buildbot <buildbot@angular2buch.de>
2021-07-06 12:53:48 +0000

    Auto-generated commit [ci skip]
    Triggered by commit: https://github.com/angular-schule/book-monkey4-src/commit/e2f18faebc0babbe5140be0c62a6ad3ddb2f9ac4
    CircleCI build: https://circleci.com/gh/angular-schule/book-monkey4-src/1169

--- src/app/shared/isbn.pipe.ts
+++ src/app/shared/isbn.pipe.ts
@@ -6,7 +6,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class IsbnPipe implements PipeTransform {
 
   transform(value: string): string {
-    if (!value) { return null; }
+    // ⚠️ Unterschied zum Buch: Leerer String statt `null`, sodass Return-Typ `string` erfüllt ist
+    if (!value) { return ''; }
     return `${value.substr(0, 3)}-${value.substr(3)}`;
   }
 }
